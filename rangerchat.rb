92be117c8e3d28f5460daabc7c04866111eacbe5
David Keane <david.keane.1974@gmail.com>
2024-09-15 23:30:38 +0100

    Uploaded .tar.gz file, changed sha256 and tried to fix brew install with dos2unix

--- rangerchat.rb
+++ rangerchat.rb
@@ -2,7 +2,7 @@ class Rangerchat < Formula
     desc "A command-line tool to interact with OpenAI's GPT-4 model"
     homepage "https://github.com/davidtkeane/rangerchat.git"
     url "https://github.com/davidtkeane/rangerchat/archive/refs/tags/v0.1.0.tar.gz"
-    sha256 "dfaf8498f5c18437f5967956dc974f3a239e4ed8d1c56038d28dfe519668dc2b"
+    sha256 "126406ea5dda23858e1945e75139d1f025566a56048e7d35759a97bd1db8e91c"
     license "MIT"
   
     depends_on "python@3.10"  # Or whatever Python version your project requires
